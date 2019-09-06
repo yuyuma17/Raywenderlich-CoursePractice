40352b4d367e2175357d49ef6947d70745b0c9be
Lacie <dark830917@gmail.com>
2019-09-06 15:48:36 +0800

    basic sliderMove change Value is ok

--- BullsEye/BullsEye/ViewController.swift
+++ BullsEye/BullsEye/ViewController.swift
@@ -12,11 +12,31 @@ class ViewController: UIViewController {
 
     var currentValue = 0
     
+    @IBOutlet weak var slider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let roundedValue = slider.value.rounded()
+        currentValue = Int(roundedValue)
     }
 
+    @IBAction func showAlert(_ sender: UIButton) {
+        
+        let message = "The value of the slider is now: \(currentValue)"
+        
+        let alert = UIAlertController(title: "Hello, World!", message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+        
+    }
+    
+    @IBAction func sliderMoved(_ slider: UISlider) {
+        let roundedValue = slider.value.rounded()
+        currentValue = Int(roundedValue)
+    }
+    
 
 }
 

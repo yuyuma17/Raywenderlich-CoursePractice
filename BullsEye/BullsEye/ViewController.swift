b5dfc6be9d0130bc5a39d480ee43d2a27fb211c8
Lacie <dark830917@gmail.com>
2019-09-06 18:02:40 +0800

    target score round Label are ok

--- BullsEye/BullsEye/ViewController.swift
+++ BullsEye/BullsEye/ViewController.swift
@@ -12,34 +12,40 @@ class ViewController: UIViewController {
 
     var currentValue = 0
     var targetValue = 0
+    var score = 0
+    var round = 0
     
     @IBOutlet weak var slider: UISlider!
     @IBOutlet weak var targetLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var roundLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let roundedValue = slider.value.rounded()
         currentValue = Int(roundedValue)
+        round = 1
         
         startNewRound()
     }
 
     @IBAction func showAlert(_ sender: UIButton) {
         
-        var difference = currentValue - targetValue
-        if difference < 0 {
-            difference = difference * -1
-        }
+        let difference = abs(currentValue - targetValue)
+        let points = 100 - difference
+        score = points
         
-        let message = "The value of the slider is now: \(currentValue)" + "\n The target value is: \(targetValue)" + "\n The difference is \(difference)"
+        let message = "Your score are \(points) points"
         
         let alert = UIAlertController(title: "Hello, World!", message: message, preferredStyle: .alert)
-        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+        let action = UIAlertAction(title: "Next Round", style: .default) { (UIAlertAction) in
+            self.startNewRound()
+        }
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
         
-        startNewRound()
+        getTheScore()
     }
     
     @IBAction func sliderMoved(_ slider: UISlider) {
@@ -52,6 +58,12 @@ class ViewController: UIViewController {
         currentValue = 50
         slider.value = Float(currentValue)
         targetLabel.text = String(targetValue)
+        round = round + 1
+    }
+    
+    func getTheScore() {
+        scoreLabel.text = String(score)
+        roundLabel.text = String(round)
     }
 
 }

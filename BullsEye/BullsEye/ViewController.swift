c4cec9c8c5c21e47016f7fd50cbd97abb651a3dd
Lacie <dark830917@gmail.com>
2019-09-06 17:07:18 +0800

    targetValue and startnewround ok

--- BullsEye/BullsEye/ViewController.swift
+++ BullsEye/BullsEye/ViewController.swift
@@ -11,25 +11,30 @@ import UIKit
 class ViewController: UIViewController {
 
     var currentValue = 0
+    var targetValue = 0
     
     @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var targetLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let roundedValue = slider.value.rounded()
         currentValue = Int(roundedValue)
+        
+        startNewRound()
     }
 
     @IBAction func showAlert(_ sender: UIButton) {
         
-        let message = "The value of the slider is now: \(currentValue)"
+        let message = "The value of the slider is now: \(currentValue)" + "\n The target value is: \(targetValue)"
         
         let alert = UIAlertController(title: "Hello, World!", message: message, preferredStyle: .alert)
         let action = UIAlertAction(title: "OK", style: .default, handler: nil)
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
         
+        startNewRound()
     }
     
     @IBAction func sliderMoved(_ slider: UISlider) {
@@ -37,6 +42,12 @@ class ViewController: UIViewController {
         currentValue = Int(roundedValue)
     }
     
+    func startNewRound() {
+        targetValue = Int.random(in: 1...100)
+        currentValue = 50
+        slider.value = Float(currentValue)
+        targetLabel.text = String(targetValue)
+    }
 
 }
 

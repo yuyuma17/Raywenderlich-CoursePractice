f47abca58c227283aff4f95993cea65ab637f553
Lacie <dark830917@gmail.com>
2019-09-09 15:04:15 +0800

    webview is ok.  fix it

--- BullsEye/BullsEye/AboutViewController.swift
+++ BullsEye/BullsEye/AboutViewController.swift
@@ -7,17 +7,27 @@
 //
 
 import UIKit
+import WebKit
 
 class AboutViewController: UIViewController {
 
+    @IBOutlet weak var webViewFrame: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        webViewFrame = WKWebView(frame: .init(x: 20, y: 20, width: 528, height: 235))
+        self.view.addSubview(webViewFrame)
+        
+        if let htmlPath = Bundle.main.path(forResource: "BullsEye", ofType: "html") {
+            let url = URL(fileURLWithPath: htmlPath)
+            let request = URLRequest(url: url)
+            webViewFrame.load(request)
+        }
     }
     
     @IBAction func backToMainScreen(_ sender: UIButton) {
         dismiss(animated: true, completion: nil)
     }
-    
 
 }
